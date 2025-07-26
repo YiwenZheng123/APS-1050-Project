a34dea3620476b852aa721c3c460e0f222705357
shshengeng <ericzhang64@yahoo.com>
2025-07-26 02:01:41 -0400

    modify the app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,12 @@ App = {
   web3Provider: null,
   contracts: {},
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
+  init: async function () {
+    $.getJSON('../pets.json', function (data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
 
-      for (i = 0; i < data.length; i ++) {
+      for (let i = 0; i < data.length; i++) {
         petTemplate.find('.panel-title').text(data[i].name);
         petTemplate.find('img').attr('src', data[i].picture);
         petTemplate.find('.pet-breed').text(data[i].breed);
@@ -24,100 +23,109 @@ App = {
     return await App.initWeb3();
   },
 
-  initWeb3: async function() {
+  initWeb3: async function () {
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+      } catch (error) {
+        console.error('User denied account access');
+      }
+    } else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
 
-    // Modern dapp browsers...
-if (window.ethereum) {
-  App.web3Provider = window.ethereum;
-  try {
-    // Request account access
-    await window.ethereum.enable();
-  } catch (error) {
-    // User denied account access...
-    console.error("User denied account access")
-  }
-}
-// Legacy dapp browsers...
-else if (window.web3) {
-  App.web3Provider = window.web3.currentProvider;
-}
-// If no injected web3 instance is detected, fall back to Ganache
-else {
-  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-}
-web3 = new Web3(App.web3Provider);
     return App.initContract();
   },
 
-  initContract: function() {
-    $.getJSON('Adoption.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
+  initContract: function () {
+    $.getJSON('Adoption.json', function (data) {
       var AdoptionArtifact = data;
       App.contracts.Adoption = TruffleContract(AdoptionArtifact);
-    
-      // Set the provider for our contract
       App.contracts.Adoption.setProvider(App.web3Provider);
-    
-      // Use our contract to retrieve and mark the adopted pets
+
       return App.markAdopted();
     });
 
     return App.bindEvents();
   },
 
-  bindEvents: function() {
+  bindEvents: function () {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '.btn-return', App.handleReturn);
   },
 
-  markAdopted: function(adopters, account) {
-    var adoptionInstance;
-
-    App.contracts.Adoption.deployed().then(function(instance) {
-      adoptionInstance = instance;
-    
-      return adoptionInstance.getAdopters.call();
-    }).then(function(adopters) {
-      for (i = 0; i < adopters.length; i++) {
+  markAdopted: function () {
+    App.contracts.Adoption.deployed().then(function (instance) {
+      return instance.getAdopters.call();
+    }).then(function (adopters) {
+      for (let i = 0; i < adopters.length; i++) {
+        const petPanel = $('.panel-pet').eq(i);
         if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+          petPanel.find('.btn-adopt').text('Adopted').attr('disabled', true);
+          petPanel.find('.btn-return').attr('disabled', false);
+        } else {
+          petPanel.find('.btn-adopt').text('Adopt').attr('disabled', false);
+          petPanel.find('.btn-return').attr('disabled', true);
         }
       }
-    }).catch(function(err) {
+    }).catch(function (err) {
       console.log(err.message);
     });
   },
 
-  handleAdopt: function(event) {
+  handleAdopt: function (event) {
     event.preventDefault();
-
     var petId = parseInt($(event.target).data('id'));
 
-    var adoptionInstance;
-
-    web3.eth.getAccounts(function(error, accounts) {
+    web3.eth.getAccounts(function (error, accounts) {
       if (error) {
         console.log(error);
+        return;
       }
-    
       var account = accounts[0];
-    
-      App.contracts.Adoption.deployed().then(function(instance) {
-        adoptionInstance = instance;
-    
-        // Execute adopt as a transaction by sending account
-        return adoptionInstance.adopt(petId, {from: account});
-      }).then(function(result) {
+
+      App.contracts.Adoption.deployed().then(function (instance) {
+        return instance.adopt(petId, { from: account });
+      }).then(function () {
         return App.markAdopted();
-      }).catch(function(err) {
+      }).catch(function (err) {
         console.log(err.message);
       });
     });
-  }
+  },
 
+  handleReturn: function (event) {
+    event.preventDefault();
+    var petId = parseInt($(event.target).data('id'));
+    console.log("Returning petId:", petId); ///////
+
+    web3.eth.getAccounts(function (error, accounts) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      var account = accounts[0];
+      console.log("From account:", account);/////
+
+      App.contracts.Adoption.deployed().then(function (instance) {
+        return instance.returnPet(petId, { from: account, value: '10000000000000000' }); 
+      }).then(function () {
+        return App.markAdopted();
+      }).catch(function (err) {
+        console.error("Return transaction error:", err.message);
+        console.error("Return transaction full error:", err);
+        alert("Return failed: " + (err.message || JSON.stringify(err)));
+      });
+    });
+  }
 };
 
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
   });
 });
